485d43f633803bfa7d8d1c59b5d9e8706cf4d288
chewieee <matt.davis129@gmail.com>
2014-06-03 22:57:31 -0700

    nailed the rpn calculator

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -1,2 +1,79 @@
 # 12_rpn_calculator
 
+class RPNCalculator
+
+	def value
+		@operands[-1]
+	end 
+
+	def initialize
+		@operands = []
+	end
+
+	def push(arg)
+		@operands.push(arg.to_f)
+	end
+
+	def plus
+		push(pop + pop)
+	end
+
+	def minus
+		value = pop
+		push(pop - value)
+	end
+
+	def divide
+		value = pop
+		push(pop / value)
+	end
+
+	def times
+		push(pop * pop)
+	end
+
+	def pop
+		raise "calculator is empty" if @operands.empty?
+		@operands.pop
+	end
+
+	def tokens(arg)
+		arg.split(' ').map do |x|
+			if %w{+ - * /}.include?(x)
+				x.to_sym
+			else
+				x.to_i
+			end
+		end
+	end
+
+	def evaluate(arg)
+		tokens(arg).each do |x|
+
+			case x
+
+			
+
+			when :*
+				times
+
+			when :+
+				plus
+
+			when :-
+				minus
+
+			when :/
+				divide
+
+			else
+				push(x)
+			end
+		end
+		value		
+	end
+
+end
+
+
+# Test Area 
